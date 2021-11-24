1e2428cbc6c9a7f666d61ee05879b40c5f5e87b0
shreyas ramadas <shreyasir@outlook.com>
2021-11-24 23:24:08 +0530

    String Reversal in CSharp

---
+++ csharp/1_String_Reversal/StringReversal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp._1_String_Reversal
+{
+    public static class StringReversal
+    {
+      public static string StringReversal_Solution1(string actual_value)
+        {
+            var char_value = actual_value.ToCharArray();
+            for (int i = 0, j = actual_value.Length - 1; i < j; i++,j--){
+                char_value[i]=actual_value[j];
+                char_value[j] = actual_value[i];
+            }
+            return new String(char_value);
+        }
+    }
+}
--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using csharp._1_String_Reversal;
+
 Console.WriteLine("Hello, World!");
+
+
+//Reverse a string
+Console.WriteLine(StringReversal.StringReversal_Solution1("APPLE"));
